447038dfdb0725debdcec483950ab20f231ab289
Dan <danielandrewvoss@gmail.com>
2016-07-07 17:29:28 -0400

    Add user repo

--- src/main/java/com/theironyard/services/UserRepository.java
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -1,7 +1,10 @@
 package com.theironyard.services;
 
+import com.theironyard.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Dan on 7/7/16.
  */
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
