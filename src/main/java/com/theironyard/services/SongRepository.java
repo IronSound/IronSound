2ddeec9f9cbf55c4d1fb69135ef573f909388444
Dan <danielandrewvoss@gmail.com>
2016-07-08 13:43:31 -0400

    Add query method

--- src/main/java/com/theironyard/services/SongRepository.java
+++ src/main/java/com/theironyard/services/SongRepository.java
@@ -1,10 +1,12 @@
 package com.theironyard.services;
 
 import com.theironyard.entities.Song;
+import com.theironyard.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Dan on 7/7/16.
  */
 public interface SongRepository extends CrudRepository<Song, Integer>{
+    public Iterable<Song> findByUser(User user);
 }
