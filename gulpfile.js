4dc47e6036e121c9ad5b3f772e0c955213e724ca
Winston Waters <Winstonw57@gmail.com>
2016-07-08 14:58:13 -0400

    gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var babel = require('gulp-babel');
+var minify = require('gulp-minify');
+var browserify = require('gulp-browserify');
+var server = require('gulp-server-livereload');
