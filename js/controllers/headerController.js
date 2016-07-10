32a05ee5584faa12d6ed4f617955a7ae5f3e460d
David Ryan Delli-Gatti <Gatti@Davids-MacBook-Pro.local>
2016-07-10 19:13:58 -0400

    speaking of tabs...

--- js/controllers/headerController.js
+++ js/controllers/headerController.js
@@ -1,6 +1,20 @@
-module.exports=function(app){
-  app.controller("headerController", ["$scope", "loginService", function ($scope, loginService){
-    $scope.name = loginService.getUserName();
-    $scope.tab = loginService.getUserTab();
-  }]);
+module.exports = function(app) {
+    app.controller("headerController", ["$scope", "loginService", function($scope, loginService) {
+        $scope.name = loginService.getUserName();
+        $scope.tab = loginService.getUserTab();
+
+// attemping to make the damn tab count
+        // $scope.counter = 0;
+        // $scope.count = function(inc) {
+        //     $scope.counter += inc;
+        // };
+
+        // var app = angular.module("myApp", []);
+        // app.controller('check', function($scope) {
+        //     // $scope.counter = 0;
+        //     // $scope.count = function(inc) {
+        //     //     $scope.counter += inc;
+        //     // };
+        // });
+    }]);
 };
