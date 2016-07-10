bb5b81459c181af1e3fb055f5a537af2e2f3c89d
Winston Waters <Winstonw57@gmail.com>
2016-07-10 15:09:35 -0400

    working on get route of songs to add to playlist

--- js/controllers/libraryController.js
+++ js/controllers/libraryController.js
@@ -1,33 +1,29 @@
 module.exports = function(app) {
     app.controller('LibraryController', ['$scope', 'libraryService', function($scope, libraryService) {
-        $scope.library = [{
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, {
-            artist: "Paul Wood"
-        }, ];
 
-        $scope.add = function() {
-            console.log("Adding the song... hopefully")
-            return libraryService.addTrack(5)
+
+        $scope.library = [{
+            artist: "Paul Wall",
+            song: "Sittin' Sideways",
+            trackId: 1,
+        }, {
+          artist: "Paul Wall",
+          song: "Sittin' Sideways",
+          trackId: 2,
+        }, {
+          artist: "Paul Wall",
+          song: "Sittin' Sideways",
+          trackId: 3,
+        },
+       ];
+
+        $scope.add = function(song) {
+            console.log("Adding the song... hopefully", song.trackId)
+            libraryService.addTrack(song.trackId);
+            $location.path('/playlist');
         }
+
+        libraryService.getPlaylist();
 
 
     }]);
